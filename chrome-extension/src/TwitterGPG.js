b21b7ae872695290d6dac292f0cc8484317a8900
Javed Khan <tuxcanfly@gmail.com>
2020-07-18 15:51:49 +0530

    chrome ext: download qr

--- chrome-extension/src/TwitterGPG.js
+++ chrome-extension/src/TwitterGPG.js
@@ -6,6 +6,16 @@ import PrivateKeyForm from './PrivateKeyForm'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCogs } from '@fortawesome/free-solid-svg-icons'
 
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
 class TwitterGPG extends Component {
   constructor (props) {
     super(props)
@@ -28,12 +38,40 @@ class TwitterGPG extends Component {
     if (!this.state.tweetMessage) { return }
     this.setState((prevState) => {
       const responseSignature = this.signTweet(prevState.tweetMessage)
-      return { ...this.state, tweetMessage: '', signature: responseSignature }
+      return { ...this.state, signature: responseSignature }
     })
   }
 
   signTweet (data) {
     return bcrypto.browser.sign(data, this.state.privateKey);
+  }
+
+  downloadQR() {
+    var filename = 'qr' + makeid(5) + '.png'
+    var canvas = document.getElementsByTagName('canvas')[0];
+
+    /// create an "off-screen" anchor tag
+    var lnk = document.createElement('a'), e;
+
+    /// the key here is to set the download attribute of the a tag
+    lnk.download = filename;
+
+    /// convert canvas content to data-uri for link. When download
+    /// attribute is set the content pointed to by link will be
+    /// pushed as "download" in HTML5 capable browsers
+    lnk.href = canvas.toDataURL("image/png;base64");
+
+    /// create a "fake" click-event to trigger the download
+    if (document.createEvent) {
+      e = document.createEvent("MouseEvents");
+      e.initMouseEvent("click", true, true, window,
+        0, 0, 0, 0, 0, false, false, false,
+        false, 0, null);
+
+      lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+      lnk.fireEvent("onclick");
+    }
   }
 
   rerenderParentCallback () {
@@ -77,6 +115,7 @@ class TwitterGPG extends Component {
             <br />
             <InputGroup.Append>
               <Button disabled={!this.state.tweetMessage} variant='primary' onClick={() => this.handleOnClick()}>Generate QR for Tweet</Button>
+              <Button disabled={!this.state.signature} variant='primary' onClick={() => this.downloadQR()}>Download QR</Button>
             </InputGroup.Append>
           </InputGroup>
         </div>
